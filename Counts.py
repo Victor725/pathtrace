a8fca9cf74b50f3d0c77d4e615842bba2ca23aab
Vic <1149833549@qq.com>
2023-06-07 19:57:17 +0800

    get addrs

--- Counts.py
+++ Counts.py
@@ -1,11 +1,29 @@
 import os
 
-out_path=""
 
-if __name__=="__main__":
-    for root, dirs, files in os.walk(out_path):
-        for file in files:
-            file=out_path+file
-            with open(file) as f:
+addr_count = {}
 
+def add_addr(addr):
+    if addr_count.has_key(addr):
+        addr_count[addr]+=1
+    else:
+        addr_count[addr]=1
+
+def get_addrs(path):
+    with open(path,'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            line = line.split(' ')
+            try:
+                i = line.index('cur_loc:')
+                addr = int(line[i+1], 16)
+                add_addr(addr)
+
+                i = line.index('target_loc:')
+                addr = int(line[i+1], 16)
+                add_addr(addr)
+            except:
+                continue
                 
+
+get_addrs("")
--- get_Cov.py
+++ get_Cov.py
@@ -3,6 +3,31 @@ import os
 input_path='/home/kali/Desktop/program+input/pdfimages/queue/'
 bincmd='/home/kali/Desktop/program+input/pdfimages/pdfimages'
 
+addr_count = {}
+
+def add_addr(addr):
+    if addr_count.has_key(addr):
+        addr_count[addr]+=1
+    else:
+        addr_count[addr]=1
+
+def get_addrs(path):
+    with open(path,'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            line = line.split(' ')
+            try:
+                i = line.index('cur_loc:')
+                addr = int(line[i+1], 16)
+                add_addr(addr)
+
+                i = line.index('target_loc:')
+                addr = int(line[i+1], 16)
+                add_addr(addr)
+            except:
+                continue
+
+
 if __name__=='__main__':
     
     out_path = input_path+'trace'
@@ -12,10 +37,16 @@ if __name__=='__main__':
 
 
     for root, dirs, files in os.walk(input_path):
-        for input_i in files:
-            if os.path.exists(out_path+'/'+input_i):
+        for file in files:
+            if os.path.exists(out_path+'/'+file):
                 continue
-            cmd = "/home/kali/Desktop/pin-3.26/pin -t obj-intel64/pathtrace.so -- "+bincmd+' '+input_i+' /dev/null > '+out_path+'/'+input_i+'.out'
+            cmd = "/home/kali/Desktop/pin-3.26/pin -t obj-intel64/pathtrace.so -- "+bincmd+' '+file+' /dev/null > '+out_path+'/'+file+'.out'
             print(cmd)
+            os.system(cmd)
+
+            #读取log
+            get_addrs(out_path+'/'+file)
+
+            cmd = 'rm '+out_path+'/'+file+'.out'
             os.system(cmd)
 
