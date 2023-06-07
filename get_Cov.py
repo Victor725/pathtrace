d5441870dfa854cad3a5c70af141fd7100426619
Vic <1149833549@qq.com>
2023-06-07 06:50:54 -0400

    no dup

--- get_Cov.py
+++ get_Cov.py
@@ -13,6 +13,8 @@ if __name__=='__main__':
 
     for root, dirs, files in os.walk(input_path):
         for input_i in files:
+            if os.path.exists(out_path+'/'+input_i):
+                continue
             cmd = "/home/kali/Desktop/pin-3.26/pin -t obj-intel64/pathtrace.so -- "+bincmd+' '+input_i+' /dev/null > '+out_path+'/'+input_i+'.out'
             print(cmd)
             os.system(cmd)
