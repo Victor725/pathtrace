6bdfc6ec3a2dfbcabc8a1ec6f0e807363854f9bf
Vic <1149833549@qq.com>
2023-06-08 04:56:29 -0400

    add addr

--- get_Cov.py
+++ get_Cov.py
@@ -3,6 +3,30 @@ import os
 input_path='/home/kali/Desktop/program+input/pdfimages/queue/'
 bincmd='/home/kali/Desktop/program+input/pdfimages/pdfimages'
 
+def add_addr(addr):
+    if addr in addr_count:
+        addr_count[addr]+=1
+    else:
+        addr_count[addr]=1
+
+def get_addrs(path):
+    with open(path,'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            line = line.split(' ')
+            try:
+                i = line.index('cur_loc:')
+                addr = int(line[i+1], 16)
+                add_addr(addr)
+
+                i = line.index('target_loc:')
+                addr = int(line[i+1], 16)
+                add_addr(addr)
+            except Exception as e:
+                #print(e)
+                continue
+    os.remove(path)
+
 if __name__=='__main__':
     
     out_path = input_path+'trace'
@@ -18,3 +42,10 @@ if __name__=='__main__':
             cmd = "/home/kali/Desktop/pin-3.26/pin -t obj-intel64/pathtrace.so -- "+bincmd+' '+input_i+' /dev/null > '+out_path+'/'+input_i+'.out'
             print(cmd)
             os.system(cmd)
+
+            get
+
+
+
+
+
