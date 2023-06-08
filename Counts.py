5b2dea89c2e36771a96f8172e47b8eebd7859427
Vic <1149833549@qq.com>
2023-06-08 04:45:57 -0400

    modify Counts

--- Counts.py
+++ Counts.py
@@ -4,7 +4,7 @@ import os
 addr_count = {}
 
 def add_addr(addr):
-    if addr_count.has_key(addr):
+    if addr in addr_count:
         addr_count[addr]+=1
     else:
         addr_count[addr]=1
@@ -22,8 +22,9 @@ def get_addrs(path):
                 i = line.index('target_loc:')
                 addr = int(line[i+1], 16)
                 add_addr(addr)
-            except:
+            except Exception as e:
+                #print(e)
                 continue
                 
 
-get_addrs("")
+get_addrs("/home/kali/Desktop/out")
